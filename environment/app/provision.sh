b27d01c589ee6b9c8457048b5cc81b9ba0ebbe94
Tech-A44 <tech-a44@Tech-A44s-Air.lan>
2018-01-29 20:34:29 +0000

    End of day 1

--- environment/app/provision.sh
+++ environment/app/provision.sh
@@ -3,3 +3,11 @@
 # update and upgrade
 sudo apt-get update -y
 sudo apt-get upgrade -y
+sudo apt-get install nginx -y
+# Install nodejs
+curl -sL https://deb.nodesource.com/setup_6.x -o nodesource_setup.sh
+sudo bash ./nodesource_setup.sh
+sudo apt-get install nodejs -y
+
+# Install pm2
+sudo npm install pm2 -g
